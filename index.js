656a87735c3d0a969c611cf45fcc0cdfc9cacec7
dinhthenhan <dinhthenhandako@gmail.com>
2025-05-30 17:57:12 +0700

    add port

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
       'http://localhost:3002',
       'http://172.16.0.2:3000',
       'http://192.168.1.50:3000',
+      'http://192.168.1.46:3000',
       // Thêm các domain thực tế của frontend khi triển khai
       // 'https://your-frontend.com',
       // 'https://<your-render-frontend>.onrender.com',
