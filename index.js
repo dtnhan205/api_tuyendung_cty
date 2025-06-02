543aaa4ac70df8ada4a127a06c46044d7bed5575
dinhthenhan <dinhthenhandako@gmail.com>
2025-06-02 08:58:24 +0700

    add port

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
       'http://localhost:3801',
       'http://localhost:3001',
       'http://192.168.41.77:3000',
+      'http://172.16.0.2:3000',
       'http://192.168.1.50:3000',
       'http://192.168.1.46:3000',
       // Thêm các domain thực tế của frontend khi triển khai
