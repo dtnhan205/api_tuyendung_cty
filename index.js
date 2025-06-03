45a90e3fd21fb9e9f93057bc5391481991dda274
dinhthenhan <dinhthenhandako@gmail.com>
2025-06-03 17:37:34 +0700

    fix

--- index.js
+++ index.js
@@ -11,16 +11,17 @@ require('dotenv').config();
 
 const app = express();
 
+// Cấu hình CORS
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(',')
   : [
-      // 'http://localhost:3000',
-      // 'http://192.168.41.77:3000',
-      // 'http://172.16.0.2:3000',
-      // 'http://192.168.1.50:3000',
-      // 'http://192.168.1.46:3000',
-      'https://api-tuyendung-cty.onrender.com'
+      'http://localhost:3000',
+      'http://192.168.41.77:3000',
+      'http://172.16.0.2:3000',
+      'http://192.168.1.50:3000',
+      'http://192.168.1.46:3000',
       // Thêm các domain thực tế của frontend khi triển khai
+      
       // 'https://your-frontend.com',
       // 'https://<your-render-frontend>.onrender.com',
     ];
