ef797a597688ed31dbdd88400f8413c0deade842
dinhthenhan <dinhthenhandako@gmail.com>
2025-06-30 15:41:09 +0700

    fix import new

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const newsController = require('../controllers/newsController');
+const newsController = require('../controllers/newController');
 const jwt = require('jsonwebtoken');
 const Admin = require('../models/admin');
 const { upload, handleMulterError } = require('../middlewares/upload');
